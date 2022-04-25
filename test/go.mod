b67ff927954137a6c5eb9d7b8ba12cad63aa50fb
XieWei <xiewei.xw@nonolive.com>
2022-04-25 17:27:20 +0800

    feat: 单参考多 Module 模式

---
+++ test/go.mod
@@ -0,0 +1,8 @@
+module github.com/XieWeiXie/self-system/test
+
+go 1.18
+
+require (
+	github.com/XieWeiXie/self-system/example v0.0.2
+	github.com/XieWeiXie/self-system/refactor-system v0.0.2
+)
--- test/main.go
+++ test/main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"fmt"
+	self2 "github.com/XieWeiXie/self-system/example/pkg/self"
+	self1 "github.com/XieWeiXie/self-system/refactor-system/pkg/self"
+)
+
 func main() {
+	hello := self1.Hello{}
+	fmt.Println(hello.Say())
+	world := self2.World{}
+	fmt.Println(world.Say())
+
 }
